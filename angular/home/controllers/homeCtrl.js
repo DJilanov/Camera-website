d6a05c541517ecfb439c1d3255c777e45043afd9
Dimitar Jilanov <dimitar.jilanov@mobilewavesolutions.com>
2015-05-13 20:50:48 +0300

    Integrate better response at the home page

--- angular/home/controllers/homeCtrl.js
+++ angular/home/controllers/homeCtrl.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('Home')
-    .controller('HomeCtrl', ['$scope', '$location',
-        function ($scope, $location) {
-			
+    .controller('HomeCtrl', ['$scope', '$location', '$http',
+        function ($scope, $location, $http) {
+        	$scope.response = null;
+			$http.get('./home.json').success(function(data, status, headers, config) {
+			    $scope.response = data;
+			  }).
+			  error(function(data, status, headers, config) {
+			    alert('Error on fetching from the server');
+			  });
 
 		}]);
